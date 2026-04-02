4d998ee9fd6fc31801e656a67293890bea6b19f9
Shubham Thakur <agastya071@gmail.com>
2026-04-02 13:39:00 +0530

    spending more time in terminal to understand  the  execution

---
+++ Docker-Fundamentals/Docker volume/learning.py
@@ -0,0 +1,16 @@
+user_name = input("enter your name to store in file or enter to proceed: ")
+if user_name:
+    with open("user_info.txt", "w") as file:
+        file.write(user_name + "\n")
+
+show_info = input("do you want to see all user names? y/n: ")   
+if show_info == 'y' :  
+    try:
+        with open('user_info.txt', 'r') as file:
+            content = file.readlines() 
+
+    except Exception as e:
+        print(e,type(e))    
+    else:
+        for line in content:
+            print(f'{line.rstrip()}')    
